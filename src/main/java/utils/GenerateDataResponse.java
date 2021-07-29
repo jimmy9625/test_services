934fa803b6fc529983f33f4875e3b5a566233947
Jaime Velasquez <jaime9625@gmail.com>
2021-07-28 23:47:18 -0500

    initial commit

---
+++ src/main/java/utils/GenerateDataResponse.java
@@ -0,0 +1,22 @@
+package utils;
+
+import models.SingleUser;
+import org.json.JSONObject;
+
+public class GenerteDataResponse {
+
+    public static String body(SingleUser info) {
+        JSONObject body = new JSONObject();
+
+        body.put("id", info.getFirst_name());
+        body.put("email", info.getEmail());
+        body.put("first_name", info.getFirst_name());
+        body.put("first_name", info.getFirst_name());
+        body.put("last_name", info.getLast_name());
+        body.put("avatar", info.getAvatar());
+
+
+
+        return body.toString();
+    }
+}
